6f511384207dfaa29bf7f890bd2bd80300005225
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2018-05-11 12:29:28 +0400

    Change detection strategy set to onPush (references checked instead of values); best to use with Immutable data type (e.g. Immutable.js).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ChangeDetectionStrategy } from '@angular/core';
 import { SharedService } from './services/shared.service';
 
 @Component({
   selector: 'root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   providers:
     //To be able to access providers (services included), we use {provide: 'tokenName', useClass: myService}
     //Then in console, do: ng.probe($0).injector.get('tokenName'); shall return myService
